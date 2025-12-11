2b97970f0627cc6089457ad8fbf49ba0cd4b881d
Niels Haupt <niels22haupt@gmail.com>
2025-12-11 21:57:20 +0100

    ?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,85 @@
 if status is-interactive
 
-    alias rm='trash-put'
-    alias l="eza --icons --git --group --time-style '+%Y-%m-%d %H:%M' --long --almost-all --mounts"
-    alias lls="eza --icons --git --group --time-style '+%Y-%m-%d %H:%M' --long --almost-all --mounts --total-size"
-    alias ls="eza --icons"
-    alias ll="eza -1 --icons"
-    alias la="eza -1 --icons --almost-all"
+    alias rm 'trash-put'
 
-    set -Ux FZF_DEFAULT_OPTS "\
+    alias l "eza --icons --git --group --time-style '+%Y-%m-%d %H:%M' --long --almost-all --mounts"
+    alias lls "eza --icons --git --group --time-style '+%Y-%m-%d %H:%M' --long --almost-all --mounts --total-size"
+    alias ls 'eza --icons'
+    alias ll 'eza -1 --icons'
+    alias la 'eza -1 --icons --almost-all'
+    alias tree 'eza --tree --icons '
+    alias treel 'eza --tree --icons --git --group --long --almost-all'
+    alias treea 'eza --tree --icons --git --group --long'
+
+
+    set -x FZF_DEFAULT_OPTS "\
 --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
 --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
 --color=selected-bg:#45475A \
 --color=border:#6C7086,label:#CDD6F4"
 
-    set -Ux LS_COLORS $(vivid generate catppuccin-mocha)
+    set -x LS_COLORS $(vivid generate catppuccin-mocha)
+    set -x MANPAGER "nvim -c 'Man!' -o -"
 
-    set -Ux MANPAGER "nvim -c 'Man!' -o -"
+    alias ..='z ..'
+    alias ...='z ../..'
+    alias ....='z ../../..'
+    alias .....='z ../../../..'
+    alias ......='z ../../../../..'
+    alias cd='z'
+    alias py='python3'
+
+    alias up='paru -Syu --noconfirm'
+
+    set -g fish_greeting
+
+    function venv
+        set venv_path ~/Documents/venv
+        if set -q VIRTUAL_ENV
+            deactivate
+        else
+            source $venv_path/bin/activate.fish
+        end
+    end
+
+    function vdl
+        yt-dlp \
+            -f "bv*+(251/mergeall[format_id~=251-])" \
+            --merge-output-format mkv \
+            --sub-langs all,-live_chat \
+            --no-write-comments \
+            --embed-subs \
+            --embed-thumbnail \
+            --audio-multistreams \
+            --embed-chapters --embed-metadata \
+            --sponsorblock-mark all \
+            --cookies-from-browser firefox \
+            --concurrent-fragments 8 \
+            --fragment-retries 20 --retry-sleep 10 \
+            -o "~/Videos/Youtube/%(uploader)s/%(title)s [%(id)s].%(ext)s" \
+            $argv
+    end
+
+    function mdl
+        yt-dlp \
+            -f "bestaudio/best" \
+            --extract-audio \
+            --audio-format flac \
+            --audio-quality 0 \
+            --no-write-comments \
+            --embed-thumbnail \
+            --write-info-json --write-description \
+            --embed-metadata \
+            --all-subs --write-subs \
+            --sub-langs all,-live_chat \
+            --cookies-from-browser firefox \
+            --concurrent-fragments 8 \
+            --fragment-retries 20 --retry-sleep 10 \
+            -o "~/Music/Youtube/%(uploader)s/%(title)s [%(id)s].%(ext)s" \
+            $argv
+    end
+
 
     zoxide init fish | source
     starship init fish | source
--- README.md
+++ README.md
@@ -2,18 +2,20 @@
 
 my linux dotfiles
 
+(install nvim seperatly: <https://github.com/Niels-py/nvim>)
+
 ## Todo
 
 - [ ] mpv
-- [ ] neofetch
-- [ ] gtk
+- [ ] fastfetch
+- [ ] gtk (<https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme>)
 - [ ] imv
 - [ ] mimeapps (xdg-open)
 
 ## Required Programs
 
 ```
-fd ripgrep fzf eza trash-cli starship zoxide vivid kvantum qt6ct
+fd ripgrep fzf eza trash-cli starship zoxide vivid kvantum qt6ct tree-sitter-cli
 ```
 
 for kvantum and qt6ct, open them and select the theme.
